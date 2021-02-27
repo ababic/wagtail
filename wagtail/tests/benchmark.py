023f2703927a7eb37a9f8e0eb7c5415180ff6006
Andy Babic <andy.babic@torchbox.com>
2021-02-27 09:36:49 +0000

    Benchmark test output improvements

--- wagtail/tests/benchmark.py
+++ wagtail/tests/benchmark.py
@@ -6,6 +6,8 @@ import tracemalloc
 
 class Benchmark():
     repeat = 10
+    output_all_measurements = False
+    output_summary = True
 
     def test(self):
         timings = []
@@ -23,5 +25,24 @@ class Benchmark():
             timings.append(end_time - start_time)
             memory_usage.append(sum([t.size for t in after_memory.compare_to(before_memory, 'filename')]))
 
-        print("time min:", min(timings), "max:", max(timings), "avg:", sum(timings) / len(timings))  # NOQA
-        print("memory min:", min(memory_usage), "max:", max(memory_usage), "avg:", sum(memory_usage) / len(memory_usage))  # NOQA
+        time_min = min(timings)
+        time_max = max(timings)
+        time_avg = sum(timings) / len(timings)
+
+        memory_min = min(memory_usage)
+        memory_max = max(memory_usage)
+        memory_avg = int(sum(memory_usage) / len(memory_usage))
+
+        if self.output_all_measurements:
+            print('\n RUN ┆       TIME ┆     MEMORY ')
+            for i in range(self.repeat):
+                print('┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄')
+                print(' {:3d} ┆ {:2.8f} ┆ {:10d}'.format(i + 1, timings[i], memory_usage[i]))
+
+        print('\n     ┆       TIME ┆     MEMORY ')
+        print('┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄')
+        print(' MIN ┆ {:2.8f} ┆ {:10d} '.format(time_min, memory_min))
+        print('┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄')
+        print(' MAX ┆ {:2.8f} ┆ {:10d} '.format(time_max, memory_max))
+        print('┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄┆┄┄┄┄┄┄┄┄┄┄┄┄')
+        print(' AVG ┆ {:2.8f} ┆ {:10d} '.format(time_avg, memory_avg))
